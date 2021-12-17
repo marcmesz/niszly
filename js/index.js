92098cd87c207591a0e55bd78aa6b9d5ddfb1754
marcmesz <meszoly.marton@gmail.com>
2021-12-17 16:54:42 +0100

    update

--- js/index.js
+++ js/index.js
@@ -1,29 +1,104 @@
-const url1 = "https://istyle.hu/blog/megszuletett-az-m1-chip"
-const url = "https://webjazz.hu"
-
+const searchChar = "ly"
+const resultsContainer = document.querySelector(".results-container")
+const results = document.querySelector(".results")
+const resultsWordCount = document.querySelector(".results-wordcount")
+const form = document.getElementById("url-form")
+const inputField = document.getElementById("url")
+const specifiedUrl = document.getElementById("specified-url")
+const spinner = document.querySelector(".img-holder")
 const connect = new XMLHttpRequest()
 
-connect.open("GET","https://corsanywhere.herokuapp.com/"+url,true)
-connect.responseType="document"
-connect.onload = ()=>{
-    if(connect.readyState==4 && connect.status==200){
-        let response = connect.responseXML.body
-        let current = textNodesUnder(response)[792].data
-        if(current.includes("más")){
-            console.log(current)
-            console.log("más")
+let wordsIncludeLy = []
+let resultsDiv = ""
+let resultCount = 0
+
+form.addEventListener("submit",(e)=>{
+    e.preventDefault()
+
+    const formData = new FormData(form)
+    const url = formData.get("url")
+    inputField.value=""
+    resetVariables()
+    if(url){
+        spinner.style.display="flex"
+        connect.open("GET","https://corsanywhere.herokuapp.com/"+url,true)
+        connect.responseType="document"
+        connect.onload = ()=>{
+            if(connect.readyState==4 && connect.status==200){
+                let response = connect.responseXML.body
+
+                spinner.style.display="none"
+                const IGNORE = ["style", "script"];
+                const walker = document.createTreeWalker(response, NodeFilter.SHOW_TEXT);
+                const pairs = [];
+                const includesLy = [];
+                let node;
+                
+                while ((node = walker.nextNode()) !== null) {
+                    const parent = node.parentNode.tagName;
+                    if (IGNORE.includes(parent)) {
+                        continue;
+                    }
+                    const value = node.nodeValue.trim();
+                    if (value.length === 0) {
+                        continue;
+                    }
+                    
+                    pairs.push([parent.toLowerCase(), value]);
+                    
+                    
+                                        
+                }
+
+                /* console.log(pairs) */
+
+                pairs.forEach(item=>{
+                    if(item[0]!=="script" && item[0]!=="style" && item[1].includes(searchChar)){
+                        includesLy.push(item[1])
+                    }
+                })
+
+                includesLy.forEach(item=>{
+                    const words = item.split(" ")
+                    words.forEach(word=>{
+                        if(word.includes(searchChar)){
+                            wordsIncludeLy.push(word)
+                        }
+                    })
+                    resultCount=wordsIncludeLy.length
+                })
+                specifiedUrl.innerText=url
+                resultsWordCount.innerText=resultCount+" words"
+                wordsIncludeLy.forEach(item=>resultsDiv+=`<div class="result">${item}</div>`)
+                results.innerHTML+=resultsDiv
+                resultsContainer.style.display="block"
+            }
         }
+        connect.onerror = ()=>{
+            spinner.style.display="none"
+            console.error(connect.status, connect.statusText)
+            alert("Error: the following link does not support crawling: \n"+url)
+        }
+        connect.send()
     }
-}
+    else{
+        alert("Please enter a valid URL")
+    }
+})
 
-connect.onerror = ()=>{
-    console.error(connect.status, connect.statusText)
-}
 
-connect.send()
+
 
 function textNodesUnder(el){
-    var n, a=[], walk=document.createTreeWalker(el,NodeFilter.SHOW_TEXT,null,false);
+    let n, a=[], walk=document.createTreeWalker(el,NodeFilter.SHOW_TEXT,null,false);
     while(n=walk.nextNode()) a.push(n);
     return a;
   }
+
+function resetVariables(){
+    resultsContainer.style.display="none"
+    wordsIncludeLy = []
+    results.innerHTML=""
+    resultsDiv = ""
+    resultCount = 0
+}
